37bd6581bdfefc99d14ddbf03841a9742bd0f35f
JUNG DAYE <jungyj0809@gmail.com>
2025-03-16 01:05:38 +0900

    feat: DeliveryStatus Enum 추가
    - 전체 배송 상태를 관리하는 Enum 추가
    - 배송 경로 상태와 분리하여 사용
    
    Resolved #7

---
+++ delivery/src/main/java/com/business/delivery/domain/entity/DeliveryStatus.java
@@ -0,0 +1,11 @@
+package com.business.delivery.domain.entity;
+
+public enum DeliveryStatus {
+
+  CANCELED,
+  WAITING_AT_HUB,
+  IN_TRANSIT_TO_HUB,
+  ARRIVED_AT_HUB,
+  OUT_FOR_DELIVERY,
+  DELIVERED
+}
