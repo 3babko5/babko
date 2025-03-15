9918f1e47b78ac1b8094c85f800f4c375066b70b
dowon0113 <dowon1235@naver.com>
2025-03-16 02:15:22 +0900

    feat: 페이징 및 정렬 기능 추가 (JpaUtil, QueryDSLConfig, QueryDslUtil)
    - JpaUtil : Spring Data JPA 기반으로 페이징 및 정렬 처리 기능 추가
    요청된 페이지 번호, 페이지 크기, 정렬 기준을 기반으로 PageRequest 객체 생성
    CREATED, UPDATED 기준으로만 정렬 가능하도록 제한
    - QueryDSLConfig : JPAQueryFactory 빈 등록하여 QueryDSL 사용 가능하도록 설정
    - QueryDslUtil : QueryDSL 기반으로 동적 정렬 기능 추가
    Pageable 정보를 기반으로 다중 정렬 지원
    OrderSpecifier<?> 를 활용하여 QueryDSL 정렬 조건 생성
    
    Resolved #23

---
+++ common/src/main/java/com/business/common/util/JpaUtil.java
@@ -0,0 +1,52 @@
+package com.business.common.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+
+public class JpaUtil {
+
+    /**
+     * 페이징 객체를 반환
+     * @param page : 요청 페이지 (1부터 시작)
+     * @param size : 한페이지에 노출될 엘리먼트 수 (10, 30, 50 이외는 모두 10으로 처리)
+     * @param orderby : 정렬 기준 엔티티의 필드 이름 (예: createdAt, updatedAt)
+     * @param sort : 정렬 순서(DESC, ASC)
+     * @return PageRequest
+     */
+    public static PageRequest getNormalPageable(Integer page, Integer size, String orderby, String sort) {
+
+        Sort sortAndOrderBy = Sort.by(OrderBy.from(orderby).fieldName);
+        sort = sort.toUpperCase();
+        sortAndOrderBy = "ASC".equals(sort) ? sortAndOrderBy.ascending() : sortAndOrderBy.descending();
+
+        return PageRequest.of(page - 1, getUsableSize(size), sortAndOrderBy);
+    }
+
+    private static int getUsableSize(int size) {
+        return switch (size) {
+            case 30 -> 30;
+            case 50 -> 50;
+            default -> 10;
+        };
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public enum OrderBy {
+        CREATED("createdAt"),
+        UPDATED("updatedAt");
+
+        private final String fieldName;
+
+        public static OrderBy from(String value) {
+            for (OrderBy type : OrderBy.values()) {
+                if (type.name().equalsIgnoreCase(value)) {
+                    return type;
+                }
+            }
+            throw new IllegalArgumentException("orderBy는 CREATED 또는 UPDATED 여야 합니다.");
+        }
+    }
+}
