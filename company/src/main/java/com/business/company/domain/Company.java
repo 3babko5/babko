31b3856241e598daab3754f08820f95a9dd0ad19
yejiscore <yejisong24@gmail.com>
2025-03-16 19:02:27 +0900

    feat: company entity 생성
    company entity 생성
    
    Resolved #31

---
+++ company/src/main/java/com/business/company/domain/Company.java
@@ -0,0 +1,35 @@
+package com.business.company.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "p_companies")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID companyId;
+
+    @Column(nullable = false)
+    private String companyName;
+
+    @Column(nullable = false)
+    private String companyAddress;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private CompanyType companyType;
+    
+    @Column(nullable = false)
+    private UUID hubId;
+
+    @Column(nullable = false)
+    private UUID companyManagerId;
+}
