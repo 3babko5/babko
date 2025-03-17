416b432cb08998a6e1e0151a701074bac062f8cf
JUNG DAYE <jungyj0809@gmail.com>
2025-03-17 17:31:31 +0900

    comment: 주석 삭제
    Resolved #34

--- delivery/src/main/java/com/business/delivery/presentation/controller/DeliveryRouteController.java
+++ delivery/src/main/java/com/business/delivery/presentation/controller/DeliveryRouteController.java
@@ -13,17 +13,6 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/delivery-routes")
 public class DeliveryRouteController {
 
-  /**
-   * 문제점
-   * 1. 다른 micro-service가 배송 생성 기능을 구현할 만큼 구현되지 않았음
-   * 2. 따라서 실제 연관된 마이크로 서비스와 통신 할 수 없음
-   * 3. 다른 마이크로서비스가 개발이 완료되어 통신이 가능하다고 하더라도 모든 마이크로 서비스를 올려야 하기 때문에
-   * 4. 배송 담당자는 배송 관련 API 테스트를 위해 연관된 모든 도메인에 데이터를 생성해야함
-   *
-   * 해결
-   * MSA
-   */
-
   private final DeliveryRouteService deliveryRouteService;
 
   //    @PreAuthorize("hasRole('MASTER')")
