e557e6479bae2eb7dd012163fb1e60f6e091759a
JUNG DAYE <jungyj0809@gmail.com>
2025-03-17 17:07:22 +0900

    feat: 배송 경로 생성 요청 DTO 추가
    - 배송 경로 생성 시 사용되는 요청 DTO 추가
    
    Resolved #34

---
+++ delivery/src/main/java/com/business/delivery/application/dto/request/CreateDeliveryRouteRequestDto.java
@@ -0,0 +1,18 @@
+package com.business.delivery.application.dto.request;
+
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateDeliveryRouteRequestDto {
+
+  private UUID deliveryId;
+  private UUID startHubId;
+  private UUID endHubId;
+}
