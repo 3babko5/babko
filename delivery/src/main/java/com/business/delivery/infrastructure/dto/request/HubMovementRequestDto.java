5fe7ee2b6fac2db4731b58bdd978dfba015d3551
JUNG DAYE <jungyj0809@gmail.com>
2025-03-17 17:03:55 +0900

    feat: 허브 이동 경로 조회 요청 DTO 추가
    - 허브 이동 경로 조회 시 사용되는 DTO 추가
    
    Resolved #34

---
+++ delivery/src/main/java/com/business/delivery/infrastructure/dto/request/HubMovementRequestDto.java
@@ -0,0 +1,17 @@
+package com.business.delivery.infrastructure.dto.request;
+
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class HubMovementRequestDto {
+
+  private UUID departureHubId;
+  private UUID arrivalHubId;
+}
