8fc60d158f66506200d8447cf88df97c2d2a52e5
AnJungGeon <gunn0128@gmail.com>
2025-03-19 03:05:09 +0900

    feat: 허브 간 이동 정보 컨트롤러 작성
    허브 간 이동 정보 등록 요청 컨트롤러 생성했습니다
    
    Resloved #71

--- hub/src/main/java/com/business/hub/presentation/HubMovementController.java
+++ hub/src/main/java/com/business/hub/presentation/HubMovementController.java
@@ -1,4 +1,35 @@
 package com.business.hub.presentation;
 
+import com.business.hub.application.dto.request.HubMovementCreateRequest;
+import com.business.hub.application.dto.response.HubMovementResponse;
+import com.business.hub.application.service.HubMovementService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/hub-movements")
+@RequiredArgsConstructor
 public class HubMovementController {
+
+    private final HubMovementService hubMovementService;
+
+    @PostMapping
+    public ResponseEntity<List<HubMovementResponse>> registerHubMovement(
+            @RequestBody @Valid HubMovementCreateRequest request,
+            Long userId) {
+
+        userId = 1111L;
+        List<HubMovementResponse> responseList = hubMovementService.registerHubMovement(request, userId);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseList);
+    }
+
 }
