f7812a9c6fd0ac4eee39a200241ca04d61a459b4
AnJungGeon <gunn0128@gmail.com>
2025-03-19 03:02:29 +0900

    feat: 허브 간 이동 정보 response 생성
    허브 간 이동 정보 response 생성 했습니다
    
    Resolved #72

---
+++ hub/src/main/java/com/business/hub/application/dto/response/HubMovementResponse.java
@@ -0,0 +1,18 @@
+package com.business.hub.application.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Getter
+@Builder
+public class HubMovementResponse {
+
+    private UUID departureHubId;
+    private UUID arrivalHubId;
+    private BigDecimal distance;
+    private int durationTime;
+
+}
