58b0d7d931f192ef1c5c1065d725ab6182e52770
AnJungGeon <gunn0128@gmail.com>
2025-03-19 03:06:11 +0900

    feat: 허브 간 이동 정보 Mapper 작성
    허브 간 이동 정보 엔티티 변환 Mapper 작성했습니다
    
    Resloved #71

--- hub/src/main/java/com/business/hub/application/mapper/HubMovementMapper.java
+++ hub/src/main/java/com/business/hub/application/mapper/HubMovementMapper.java
@@ -1,4 +1,28 @@
 package com.business.hub.application.mapper;
 
+import com.business.hub.application.dto.response.HubMovementResponse;
+import com.business.hub.domain.entity.HubMovement;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class HubMovementMapper {
+
+    public static HubMovementResponse toHubMovementResponse(HubMovement hubMovement) {
+        return HubMovementResponse.builder()
+                .departureHubId(hubMovement.getDepartureHub().getHubId())
+                .arrivalHubId(hubMovement.getArrivalHub().getHubId())
+                .distance(hubMovement.getDistance())
+                .durationTime(hubMovement.getDurationTime())
+                .build();
+    }
+
+    public static List<HubMovementResponse> toDtoList(List<HubMovement> hubMovements) {
+        if (hubMovements == null || hubMovements.isEmpty()) {
+            return List.of();
+        }
+        return hubMovements.stream()
+                .map(HubMovementMapper::toHubMovementResponse)
+                .collect(Collectors.toList());
+    }
 }
