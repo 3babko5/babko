cd4fe61a87bcc5a877af364e98cb2ff5cb1527ef
nodaji <sniper.nhj@gmail.com>
2025-03-18 20:32:43 +0900

    feat: OpenFeign 추가 및 패키지 스캔 범위 확장
    common 모듈 적용
    CommonApplication 삭제
    gradle 추가 (openfeign)
    scanBasePackages 적용
    
    Resolved #82

--- user/src/main/java/com/business/user/UserApplication.java
+++ user/src/main/java/com/business/user/UserApplication.java
@@ -2,8 +2,10 @@ package com.business.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@EnableFeignClients
+@SpringBootApplication(scanBasePackages = {"com.business.common", "com.business.user"})
 public class UserApplication {
 
     public static void main(String[] args) {
