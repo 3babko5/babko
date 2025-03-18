08589083701bc8e1a1fb457787f104e01b9ee087
JUNG DAYE <jungyj0809@gmail.com>
2025-03-19 02:54:29 +0900

    feat: 필드 및 순서 업데이트 기능 추가
    - `assignAt`, `deliveryRouteId`, `routeSequence` 필드 추가
    - `assignToRoute 메서드 추가
      - 배송 담당자 배정 시 경로 및 배정 시간을 업데이트하도록 변경
    
    Resolved #93

--- user/src/main/java/com/business/user/domain/entity/DeliveryDriver.java
+++ user/src/main/java/com/business/user/domain/entity/DeliveryDriver.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
 import java.util.UUID;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -50,7 +51,16 @@ public class DeliveryDriver extends BaseDataEntity {
   @Comment("배송 담당자 배정 순서")
   private Long deliverySequence;
 
-  public static DeliveryDriver create(Long userId, UUID hubId, UUID slackId, DriverType driverType, Long deliverySequence) {
+  @Comment("배정 할당 시간")
+  private LocalDateTime assignAt;
+
+  @Comment("배정된 배송 경로 ID")
+  private UUID deliveryRouteId;
+
+  @Comment("배정된 배송 경로 순서")
+  private Long routeSequence;
+
+  public static DeliveryDriver create(Long userId, UUID hubId, UUID slackId, DriverType driverType, Long deliverySequence, UUID deliveryRouteId, Long routeSequence) {
 
     return DeliveryDriver.builder()
         .deliveryDriverId(userId)
@@ -58,6 +68,20 @@ public class DeliveryDriver extends BaseDataEntity {
         .slackId(slackId)
         .driverType(driverType)
         .deliverySequence(deliverySequence)
+        .deliveryRouteId(deliveryRouteId)
+        .routeSequence(routeSequence)
         .build();
   }
+
+  public void assignToRoute(UUID deliveryRouteId, Long routeSequence) {
+    this.deliveryRouteId = deliveryRouteId;
+    this.routeSequence = routeSequence;
+    this.assignAt = LocalDateTime.now();
+  }
+
+  public void updateAssignAt(LocalDateTime assignAt) {
+    this.assignAt = assignAt;
+  }
 }
+
+
