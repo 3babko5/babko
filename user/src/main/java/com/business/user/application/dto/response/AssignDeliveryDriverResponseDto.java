5a4e5b540671a9480c186cd5233e8472f080c9c1
JUNG DAYE <jungyj0809@gmail.com>
2025-03-19 02:57:53 +0900

    feat: 배정된 담당자 정보 반환
    Resolved #93

---
+++ user/src/main/java/com/business/user/application/dto/response/AssignDeliveryDriverResponseDto.java
@@ -0,0 +1,23 @@
+package com.business.user.application.dto.response;
+
+import com.business.user.domain.entity.DriverType;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AssignDeliveryDriverResponseDto {
+
+  private Long deliveryDriverId;
+  private DriverType driverType;
+  private UUID hubId;
+  private UUID deliveryRouteId;
+  private Long routeSequence;
+  private LocalDateTime assignAt;
+}
