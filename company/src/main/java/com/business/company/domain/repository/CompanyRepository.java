ce5a984f0095610d60414a3436250f4321301061
yejiscore <yejisong24@gmail.com>
2025-03-19 23:09:34 +0900

    refactor: repository 계층 구조 변경
    도메인 repository의 jpa 상속을 제거하고 infrastructure 를 생성하여 jpa 관리하도록 변경하였습니다.
    
    Resolved #103

--- company/src/main/java/com/business/company/domain/repository/CompanyRepository.java
+++ company/src/main/java/com/business/company/domain/repository/CompanyRepository.java
@@ -1,10 +1,7 @@
 package com.business.company.domain.repository;
 
 import com.business.company.domain.entity.Company;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
-
-public interface CompanyRepository extends JpaRepository<Company, UUID> {
+public interface CompanyRepository {
+    Company save(Company company);
 }
-
