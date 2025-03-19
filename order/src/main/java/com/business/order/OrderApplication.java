3fdeb0d9f03cff11221b1d30fd6322126b05a243
dowon0113 <dowon1235@naver.com>
2025-03-19 15:47:16 +0900

    feat: OrderApplication에서 패키지 스캔 범위 지정
    - `@SpringBootApplication(scanBasePackages = {"com.business.common", "com.business.order"})` 추가
    - 스캔 대상 패키지를 명시하여 빈(Bean) 등록 문제 방지
    
    Resolved #78

--- order/src/main/java/com/business/order/OrderApplication.java
+++ order/src/main/java/com/business/order/OrderApplication.java
@@ -3,7 +3,7 @@ package com.business.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.business.common", "com.business.order"})
 public class OrderApplication {
 
     public static void main(String[] args) {
