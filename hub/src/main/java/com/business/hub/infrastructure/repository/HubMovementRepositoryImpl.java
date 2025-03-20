2d77318dc007b7e7d7a235765f5893c79fb58d6d
AnJungGeon <gunn0128@gmail.com>
2025-03-20 23:57:28 +0900

    feat: 허브 간 이동 정보 조회 쿼리 작성
    허브 간 이동 정보 조회 쿼리 작성 했습니다
    
    Resloved #144

--- hub/src/main/java/com/business/hub/infrastructure/repository/HubMovementRepositoryImpl.java
+++ hub/src/main/java/com/business/hub/infrastructure/repository/HubMovementRepositoryImpl.java
@@ -1,9 +1,16 @@
 package com.business.hub.infrastructure.repository;
 
+import com.business.hub.application.dto.response.HubMovementResponse;
 import com.business.hub.domain.entity.HubMovement;
 import com.business.hub.domain.repository.HubMovementRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 @RequiredArgsConstructor
@@ -14,5 +21,30 @@ public class HubMovementRepositoryImpl implements HubMovementRepository {
     @Override
     public void save(HubMovement hubMovement) {
         hubMovementJpaRepository.save(hubMovement);
+    }
+
+    @Override
+    public Optional<HubMovement> findByHubMovementIdAndDeletedAtIsNullAndDeletedByIsNull(UUID hubmovementId) {
+        return hubMovementJpaRepository.findByHubMovementIdAndDeletedAtIsNullAndDeletedByIsNull(hubmovementId);
+    }
+
+    @Override
+    public Page<HubMovement> findAllByDeletedAtIsNullAndDeletedByIsNull(Pageable pageable) {
+        return hubMovementJpaRepository.findAllByDeletedAtIsNullAndDeletedByIsNull(pageable);
+    }
+
+    @Override
+    public List<HubMovement> findByDepartureHub_HubIdAndDeletedByIsNullAndDeletedAtIsNull(UUID departureHubId) {
+        return hubMovementJpaRepository.findByDepartureHub_HubIdAndDeletedByIsNullAndDeletedAtIsNull(departureHubId);
+    }
+
+    @Override
+    public List<HubMovement> findByArrivalHub_HubIdAndDeletedByIsNullAndDeletedAtIsNull(UUID arrivalHubId) {
+        return hubMovementJpaRepository.findByArrivalHub_HubIdAndDeletedByIsNullAndDeletedAtIsNull(arrivalHubId);
+    }
+
+    @Override
+    public Optional<HubMovement> findByDepartureHub_HubIdAndArrivalHub_HubIdAndDeletedAtIsNullAndDeletedByIsNull(UUID depatureHubId, UUID arrivalHubId) {
+        return hubMovementJpaRepository.findByDepartureHub_HubIdAndArrivalHub_HubIdAndDeletedAtIsNullAndDeletedByIsNull(depatureHubId, arrivalHubId);
     }
 }
