f3a7c6c0d67f076b0b1ee0a29aca110cc651e3f5
dowon0113 <dowon1235@naver.com>
2025-03-20 20:06:06 +0900

    feat: 배송 서비스 FeignClient 추가 및 주문 생성 시 배송 요청 연동
    - DeliveryFeignClient를 추가하여 배송 서비스와 통신 가능하도록 설정
    - 주문(Order) 생성 시 배송 생성 요청을 FeignClient를 통해 전달
    - 배송 요청 실패 시 예외 처리 (BusinessLogicException 추가)
    - 더미 데이터를 사용하여 supplierId와 productPrice 설정
    
    Resolved #127

--- order/src/main/java/com/business/order/OrderApplication.java
+++ order/src/main/java/com/business/order/OrderApplication.java
@@ -2,7 +2,9 @@ package com.business.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients(basePackages = "com.business")
 @SpringBootApplication(scanBasePackages = {"com.business.common", "com.business.order"})
 public class OrderApplication {
 
