13f6eeae5e9673bcd001b6174454ea9c1062be04
JUNG DAYE <jungyj0809@gmail.com>
2025-03-23 04:13:22 +0900

    feat: 배송 ID 기반 삭제 메서드 구현
    Resolved #174

--- delivery/src/main/java/com/business/delivery/infrastructure/repository/DeliveryJpaRepository.java
+++ delivery/src/main/java/com/business/delivery/infrastructure/repository/DeliveryJpaRepository.java
@@ -1,9 +1,11 @@
 package com.business.delivery.infrastructure.repository;
 
 import com.business.delivery.domain.entity.Delivery;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DeliveryJpaRepository extends JpaRepository<Delivery, UUID> {
 
+    Optional<Delivery> findByDeliveryIdAndDeletedAtIsNull(UUID deliveryId);
 }
