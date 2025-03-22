7633443236daab86acf8c22ef9636785e7f46ebc
AnJungGeon <gunn0128@gmail.com>
2025-03-23 00:16:18 +0900

    feat: 허브 검색요청 request 생성
    허브 검색에 필요한 request 생성 했습니다
    
    Resolved: #188

---
+++ hub/src/main/java/com/business/hub/application/dto/request/HubSearchRequest.java
@@ -0,0 +1,12 @@
+package com.business.hub.application.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HubSearchRequest {
+
+    private String hubName;
+    private String hubAddress;
+}
