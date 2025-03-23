dbd7d5ee574bf460b1523ef75b5c41e25c661455
dowon0113 <dowon1235@naver.com>
2025-03-23 19:38:25 +0900

    feat: 주문 취소를 위해 배송id 저장
    - 배송 정보 조회하여 일치하는 orderId를 필터링하여 deliveryId를 추출한 뒤 오더에 저장
    - 실패 시 예외처리
    
    Resolved #189

--- order/src/main/java/com/business/order/application/exception/OrderExceptionCode.java
+++ order/src/main/java/com/business/order/application/exception/OrderExceptionCode.java
@@ -15,7 +15,8 @@ public enum OrderExceptionCode implements ExceptionCode {
     PRODUCT_QUANTITY_EXCEEDED(HttpStatus.BAD_REQUEST, "해당상품이 품절되었습니다."),
     ORDER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 주문은 없는 주문입니다"),
     ORDER_CANNOT_BE_CANCELED(HttpStatus.BAD_REQUEST, "배송이 시작되어 주문을 취소할 수 없습니다."),
-    COMPANY_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 업체입니다.")
+    COMPANY_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 업체입니다."),
+    DELIVERY_ID_NOT_FOUND(HttpStatus.NOT_FOUND,"배송이 생성되지 않았습니다.")
 
     ;
 
--- order/src/main/java/com/business/order/domain/repository/OrderRepository.java
+++ order/src/main/java/com/business/order/domain/repository/OrderRepository.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface OrderRepository {
+    Order saveAndFlush(Order order);
+
     Order save(Order order);
 
     Order findByOrderIdWithItems(UUID orderId);
