887dc88bbad96f0d2e33262861d6f0abd009fbae
JUNG DAYE <jungyj0809@gmail.com>
2025-03-23 15:56:52 +0900

    refactor: findBydeliveryDriverId 메서드 명확화 및 Soft Delete 적용
    - deliveryDriverId 기반 조회 시 deletedAt IS NULL 조건 추가
    
    Resolved #194

--- user/src/main/java/com/business/user/deliverydriver/infrastructure/repository/DeliveryDriverRepositoryImpl.java
+++ user/src/main/java/com/business/user/deliverydriver/infrastructure/repository/DeliveryDriverRepositoryImpl.java
@@ -22,113 +22,113 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class DeliveryDriverRepositoryImpl implements DeliveryDriverRepository {
 
-  private final DeliveryDriverJpaRepository deliveryDriverJpaRepository;
-  private final EntityManager entityManager;
+    private final DeliveryDriverJpaRepository deliveryDriverJpaRepository;
+    private final EntityManager entityManager;
 
-  @Override
-  public Optional<Long> findLastDeliverySequenceForHubDrivers() {
-    return deliveryDriverJpaRepository.findLastDeliverySequenceForHubDrivers();
-  }
-
-  @Override
-  public Optional<Long> findLastDeliverySequenceForCompanyDrivers(UUID hubId) {
-    return deliveryDriverJpaRepository.findLastDeliverySequenceForCompanyDrivers(hubId);
-  }
-
-  @Override
-  public long countByDriverType(DriverType driverType) {
-    return deliveryDriverJpaRepository.countByDriverType(driverType);
-  }
-
-  @Override
-  public long countByHubIdAndDriverType(UUID hubId, DriverType driverType) {
-    return deliveryDriverJpaRepository.countByHubIdAndDriverType(hubId, driverType);
-  }
-
-  @Override
-  public Optional<DeliveryDriver> findLastAssignedDriver() {
-    return deliveryDriverJpaRepository.findLastAssignedDriver();
-  }
-
-  @Override
-  public Optional<DeliveryDriver> findNextAvailableDriver(Long currentSequence) {
-    return deliveryDriverJpaRepository.findNextAvailableDriver(currentSequence);
-  }
-
-  @Override
-  public Optional<DeliveryDriver> findFirstAvailableDriver() {
-    return deliveryDriverJpaRepository.findFirstAvailableDriver();
-  }
-
-  @Override
-  public Optional<DeliveryDriver> findLastAssignedDriverByTypeAndHub(DriverType driverType, UUID hubId) {
-    return deliveryDriverJpaRepository.findLastAssignedDriverByTypeAndHub(driverType, hubId);
-  }
-
-  @Override
-  public Optional<DeliveryDriver> findNextAvailableDriverByTypeAndHub(Long currentSequence, DriverType driverType, UUID hubId) {
-    return deliveryDriverJpaRepository.findNextAvailableDriverByTypeAndHub(currentSequence, driverType, hubId);
-  }
-
-  @Override
-  public boolean existsById(Long deliveryDriverId) {
-    return deliveryDriverJpaRepository.existsByDeliveryDriverIdAndDeletedAtIsNull(deliveryDriverId);
-  }
-
-  @Override
-  public Optional<DeliveryDriver> findById(Long deliveryDriverId) {
-    return deliveryDriverJpaRepository.findByDeliveryDriverIdAndDeletedAtIsNull(deliveryDriverId);
-  }
-
-  @Override
-  public DeliveryDriver save(DeliveryDriver deliveryDriver) {
-    return deliveryDriverJpaRepository.save(deliveryDriver);
-  }
-
-  public Page<DeliveryDriver> findDeliveryDrivers(DeliveryDriverSearchRequestDto request, Pageable pageable) {
-
-    JPAQueryFactory queryFactory = new JPAQueryFactory(entityManager);
-
-    QDeliveryDriver driver = QDeliveryDriver.deliveryDriver;
-
-    BooleanBuilder builder = new BooleanBuilder();
-
-    builder.and(driver.deletedAt.isNull());
-
-    if (request.getDeliveryDriverId() != null) {
-      builder.and(driver.deliveryDriverId.eq(request.getDeliveryDriverId()));
+    @Override
+    public Optional<Long> findLastDeliverySequenceForHubDrivers() {
+        return deliveryDriverJpaRepository.findLastDeliverySequenceForHubDrivers();
     }
 
-    if (request.getHubId() != null) {
-      builder.and(driver.hubId.eq(request.getHubId()));
+    @Override
+    public Optional<Long> findLastDeliverySequenceForCompanyDrivers(UUID hubId) {
+        return deliveryDriverJpaRepository.findLastDeliverySequenceForCompanyDrivers(hubId);
     }
 
-    if (request.getSlackId() != null) {
-      builder.and(driver.slackId.eq(request.getSlackId()));
+    @Override
+    public long countByDriverType(DriverType driverType) {
+        return deliveryDriverJpaRepository.countByDriverType(driverType);
     }
 
-    if (request.getDriverType() != null) {
-      builder.and(driver.driverType.eq(request.getDriverType()));
+    @Override
+    public long countByHubIdAndDriverType(UUID hubId, DriverType driverType) {
+        return deliveryDriverJpaRepository.countByHubIdAndDriverType(hubId, driverType);
     }
 
-    if (request.getDeliverySequence() != null) {
-      builder.and(driver.deliverySequence.eq(request.getDeliverySequence()));
+    @Override
+    public Optional<DeliveryDriver> findLastAssignedDriver() {
+        return deliveryDriverJpaRepository.findLastAssignedDriver();
     }
 
-    List<DeliveryDriver> content = queryFactory
-        .selectFrom(driver)
-        .where(builder)
-        .offset(pageable.getOffset())
-        .limit(pageable.getPageSize())
-        .orderBy(QueryDslUtil.getAllOrderSpecifierArr(pageable, driver))
-        .fetch();
+    @Override
+    public Optional<DeliveryDriver> findNextAvailableDriver(Long currentSequence) {
+        return deliveryDriverJpaRepository.findNextAvailableDriver(currentSequence);
+    }
 
-    Long total = queryFactory
-        .select(driver.count())
-        .from(driver)
-        .where(builder)
-        .fetchOne();
+    @Override
+    public Optional<DeliveryDriver> findFirstAvailableDriver() {
+        return deliveryDriverJpaRepository.findFirstAvailableDriver();
+    }
 
-    return PageableExecutionUtils.getPage(content, pageable, () -> total == null ? 0 : total);
-  }
+    @Override
+    public Optional<DeliveryDriver> findLastAssignedDriverByTypeAndHub(DriverType driverType, UUID hubId) {
+        return deliveryDriverJpaRepository.findLastAssignedDriverByTypeAndHub(driverType, hubId);
+    }
+
+    @Override
+    public Optional<DeliveryDriver> findNextAvailableDriverByTypeAndHub(Long currentSequence, DriverType driverType, UUID hubId) {
+        return deliveryDriverJpaRepository.findNextAvailableDriverByTypeAndHub(currentSequence, driverType, hubId);
+    }
+
+    @Override
+    public boolean existsById(Long deliveryDriverId) {
+        return deliveryDriverJpaRepository.existsByDeliveryDriverIdAndDeletedAtIsNull(deliveryDriverId);
+    }
+
+    @Override
+    public Optional<DeliveryDriver> findBydeliveryDriverId(Long deliveryDriverId) {
+        return deliveryDriverJpaRepository.findByDeliveryDriverIdAndDeletedAtIsNull(deliveryDriverId);
+    }
+
+    @Override
+    public DeliveryDriver save(DeliveryDriver deliveryDriver) {
+        return deliveryDriverJpaRepository.save(deliveryDriver);
+    }
+
+    public Page<DeliveryDriver> findDeliveryDrivers(DeliveryDriverSearchRequestDto request, Pageable pageable) {
+
+        JPAQueryFactory queryFactory = new JPAQueryFactory(entityManager);
+
+        QDeliveryDriver driver = QDeliveryDriver.deliveryDriver;
+
+        BooleanBuilder builder = new BooleanBuilder();
+
+        builder.and(driver.deletedAt.isNull());
+
+        if (request.getDeliveryDriverId() != null) {
+            builder.and(driver.deliveryDriverId.eq(request.getDeliveryDriverId()));
+        }
+
+        if (request.getHubId() != null) {
+            builder.and(driver.hubId.eq(request.getHubId()));
+        }
+
+        if (request.getSlackId() != null) {
+            builder.and(driver.slackId.eq(request.getSlackId()));
+        }
+
+        if (request.getDriverType() != null) {
+            builder.and(driver.driverType.eq(request.getDriverType()));
+        }
+
+        if (request.getDeliverySequence() != null) {
+            builder.and(driver.deliverySequence.eq(request.getDeliverySequence()));
+        }
+
+        List<DeliveryDriver> content = queryFactory
+            .selectFrom(driver)
+            .where(builder)
+            .offset(pageable.getOffset())
+            .limit(pageable.getPageSize())
+            .orderBy(QueryDslUtil.getAllOrderSpecifierArr(pageable, driver))
+            .fetch();
+
+        Long total = queryFactory
+            .select(driver.count())
+            .from(driver)
+            .where(builder)
+            .fetchOne();
+
+        return PageableExecutionUtils.getPage(content, pageable, () -> total == null ? 0 : total);
+    }
 }
