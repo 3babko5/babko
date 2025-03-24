02cf03625cc063828c48e7090ed15d6992c2d20f
dowon0113 <dowon1235@naver.com>
2025-03-24 10:32:54 +0900

    bugfix: feignClient 응답 구조 통일
    - 상품과 응답dto를 동일하게 구성
    - 필요한 값만 커스텀해서 주문에서 사용
    
    Resolved #231

--- order/src/main/java/com/business/order/infrastructure/client/ProductFeignClient.java
+++ order/src/main/java/com/business/order/infrastructure/client/ProductFeignClient.java
@@ -1,18 +1,26 @@
 package com.business.order.infrastructure.client;
 
-import com.business.order.infrastructure.dto.response.ProductDetailResponseDto;
+import com.business.order.application.dto.response.ProductDetailResponseDto;
+import com.business.order.infrastructure.dto.response.GetProductInfoResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.UUID;
 
-@FeignClient(name = "product-service", url = "http://delivery-service/api/v1/products")
+@FeignClient(name = "product-service", url = "http://product-service/api/v1/products")
 public interface ProductFeignClient {
 
     @GetMapping
-    ProductDetailResponseDto getProductDetail(@ModelAttribute ProductDetailResponseDto request);
+    GetProductInfoResponseDto searchProduct(
+            @RequestParam("productId") UUID productId,
+            @RequestParam(value = "productName", required = false) String productName,
+            @RequestParam(value = "companyId", required = false) UUID companyId,
+            @RequestParam("page") int page,
+            @RequestParam("size") int size,
+            @RequestParam("orderBy") String orderBy,
+            @RequestParam("sort") String sort
+    );
 
 }
