5598c589bfca62a6389d82ded8473206b798dd35
dowon0113 <dowon1235@naver.com>
2025-03-24 10:31:53 +0900

    bugfix: feignClient 응답 구조 통일
    - 업체, 배송과 응답dto를 동일하게 구성
    - 필요한 값만 커스텀해서 주문에서 사용
    
    Resolved #231

--- order/src/main/java/com/business/order/infrastructure/client/CompanyFeignClient.java
+++ order/src/main/java/com/business/order/infrastructure/client/CompanyFeignClient.java
@@ -1,7 +1,7 @@
 package com.business.order.infrastructure.client;
 
 import com.business.order.domain.entity.CompanyType;
-import com.business.order.infrastructure.dto.response.hubIdResponseDto;
+import com.business.order.infrastructure.dto.response.GetCompanyInfoResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,9 +9,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient(name = "company-service", url = "http://company-service/api/v1/companies")
 public interface CompanyFeignClient {
 
-    @GetMapping("/search")
-    hubIdResponseDto searchCompanies(
-            @RequestParam("companyType") CompanyType companyType
+    @GetMapping
+    GetCompanyInfoResponseDto searchCompanies(
+            @RequestParam(value = "companyName", required = false) String companyName,
+            @RequestParam(value = "companyType", required = false) CompanyType companyType,
+            @RequestParam("page") int page,
+            @RequestParam("size") int size,
+            @RequestParam("orderBy") String orderBy,
+            @RequestParam("sort") String sort
     );
 
 }
--- order/src/main/java/com/business/order/infrastructure/client/DeliveryFeignClient.java
+++ order/src/main/java/com/business/order/infrastructure/client/DeliveryFeignClient.java
@@ -2,7 +2,9 @@ package com.business.order.infrastructure.client;
 
 import com.business.order.infrastructure.dto.request.OrderDeliveryRequestDto;
 import com.business.order.infrastructure.dto.response.DeliveryIdResponseDto;
+import com.business.order.infrastructure.dto.response.GetDeliveryCreateResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,7 +14,7 @@ import java.util.UUID;
 public interface DeliveryFeignClient {
 
     @PostMapping
-    boolean createDelivery(@RequestBody OrderDeliveryRequestDto request);
+    GetDeliveryCreateResponseDto createDelivery(@RequestBody OrderDeliveryRequestDto request);
 
     @GetMapping("/{orderId}/deliveryStatus")//url 확인 필요
     String getDeliveryStatus(@PathVariable("orderId") UUID orderId);
