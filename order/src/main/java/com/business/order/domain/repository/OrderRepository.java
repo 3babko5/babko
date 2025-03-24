21261203d67bd0cc8db4271ed45832110f02a905
dowon0113 <dowon1235@naver.com>
2025-03-25 05:17:43 +0900

    feat: 동적 쿼리 및 페이징 기능
    - 주문 ID와 주문 상태를 조건으로 주문 목록을 검색할 수 있는 기능 추가
    - QueryDSL을 활용한 동적 쿼리 및 페이징 처리 포함
    
    Resolved #192

--- order/src/main/java/com/business/order/domain/repository/OrderRepository.java
+++ order/src/main/java/com/business/order/domain/repository/OrderRepository.java
@@ -1,12 +1,16 @@
 package com.business.order.domain.repository;
 
 import com.business.order.domain.entity.Order;
+import com.business.order.domain.entity.OrderStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface OrderRepository {
-    Order saveAndFlush(Order order);
+
+    Page<Order> search(UUID orderId, OrderStatus orderStatus, Pageable pageable);
 
     Order save(Order order);
 
